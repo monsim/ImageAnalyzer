f13c215ec7d0ab0f9e4e21de5f0c7a53c52dff5e
monsim <monsi5pen@gmail.com>
2017-10-08 10:30:11 -0700

    making call, but error 400

--- calhacks1/ViewController.swift
+++ calhacks1/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
@@ -14,11 +15,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var apiButton: UIButton!
     
     @IBAction func sendRequest(_ sender: UIButton) {
-//        print("in the method")
+        print("in the method")
 //        let config = URLSessionConfiguration.default // Session Configuration
 //        let session = URLSession(configuration: config) // Load configuration into Session
-//        let url = URL(string: "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAKTabFxyAbqV8W1zSVa-I9SN2HTowL3kE")!
-//
+        //let url = URL(string: "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAKTabFxyAbqV8W1zSVa-I9SN2HTowL3kE")!
+
+        let jsonObject: [String: Any] = [
+            "requests": [
+                "image": [
+                    "source": [
+                        "imageUri": "https://gfp-2a3tnpzj.stackpathdns.com/wp-content/uploads/2017/06/Blaze-m1-300x300.jpg"
+                    ]
+                ],
+                "features": [
+                    "type": "LABEL_DETECTION"
+                ]
+            ]
+    
+        ]
+        
+//        let trimmed = jsonObject.stringByTrimmingCharactersInSet(NSCharacterSet.whitespacesAndNewlineCharacterSet())
+        let jsonData = try? JSONSerialization.data(withJSONObject: jsonObject)
+        let url = URL(string: "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAKTabFxyAbqV8W1zSVa-I9SN2HTowL3kE")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        request.httpBody = jsonData
+        
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let responseJSON = responseJSON as? [String: Any] {
+                print(responseJSON)
+            }
+        }
+        task.resume()
+        
 //        print("one")
 //        let task = session.dataTask(with: url, completionHandler: {
 //            (data, response, error) in
@@ -34,7 +70,6 @@ class ViewController: UIViewController {
 //                    if let json = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
 //                    {
 //                        print("five")
-//                        //Implement your logic
 //                        print(json)
 //
 //                    }
@@ -52,41 +87,51 @@ class ViewController: UIViewController {
 //        })
 //        task.resume()
         
-        let urlOne = URL(string: "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAKTabFxyAbqV8W1zSVa-I9SN2HTowL3kE")!
-        var request = URLRequest(url: urlOne)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue(Bundle.main.bundleIdentifier ?? "", forHTTPHeaderField: "X-Ios-Bundle-Identifier")
-        
-        // Build our API request
-        let jsonRequest = [
-            "requests": [
-                "image": [
-                    "content": "https://gfp-2a3tnpzj.stackpathdns.com/wp-content/uploads/2017/06/Blaze-m1-300x300.jpg"
-                ],
-                "features": [
-                    [
-                        "type": "LABEL_DETECTION",
-                        "maxResults": 10
-                    ],
-                    [
-                        "type": "FACE_DETECTION",
-                        "maxResults": 10
-                    ]
-                ]
-            ]
-        ]
-        let jsonObject = JSON(jsonDictionary: jsonRequest)
-        
-        // Serialize the JSON
-        guard let data = try? jsonObject.rawData() else {
-            return
-        }
-        
-        request.httpBody = data
-        
-        // Run the request on a background thread
-        DispatchQueue.global().async { self.runRequestOnBackgroundThread(request) }
+//        let urlOne = URL(string: "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAKTabFxyAbqV8W1zSVa-I9SN2HTowL3kE")!
+//        var request = URLRequest(url: urlOne)
+//        request.httpMethod = "POST"
+//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+//        request.addValue(Bundle.main.bundleIdentifier ?? "", forHTTPHeaderField: "X-Ios-Bundle-Identifier")
+//
+//        // Build our API request
+//        let jsonRequest = [
+//            "requests": [
+//                "image": [
+//                    "content": "https://gfp-2a3tnpzj.stackpathdns.com/wp-content/uploads/2017/06/Blaze-m1-300x300.jpg"
+//                ],
+//                "features": [
+//                    [
+//                        "type": "LABEL_DETECTION",
+//                        "maxResults": 10
+//                    ],
+//                    [
+//                        "type": "FACE_DETECTION",
+//                        "maxResults": 10
+//                    ]
+//                ]
+//            ]
+//        ]
+//        //let jsonObject = JSON(jsonDictionary: jsonRequest)
+//        let json = try? JSONSerialization.jsonObject(jsonRequest)
+//
+//        if let data = data,
+//            let json = try? JSONSerialization.jsonObject(with: #imageLiteral(resourceName: <#T##String#>), options: []) as? [String: Any] {
+//            for case let result in json["results"] {
+//                if let restaurant = Restaurant(json: result) {
+//                    restaurants.append(restaurant)
+//                }
+//            }
+//        }
+//
+//        // Serialize the JSON
+//        guard let data = try? jsonObject.rawData() else {
+//            return
+//        }
+//
+//        request.httpBody = data
+//
+//        // Run the request on a background thread
+//        DispatchQueue.global().async { self.runRequestOnBackgroundThread(request) }
     }
     
     @IBAction func apiButton(_ sender: Any) {
@@ -116,12 +161,8 @@ class ViewController: UIViewController {
                 } catch {
                     
                     print("error in JSONSerialization")
-                    
                 }
-                
-                
             }
-            
         })
         task.resume()
     }
